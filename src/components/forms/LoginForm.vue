eda14100cbf99e2d42f3376a911e9f5b16f8d11d
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-19 14:00:06 +0200

    general style and UserView

--- src/components/forms/LoginForm.vue
+++ src/components/forms/LoginForm.vue
@@ -1,22 +1,34 @@
 <script setup></script>
 
 <template>
-  <form class="border p-4 rounded">
-    <div class="mb-3">
-      <label for="inputEmail" class="form-label">Email</label>
-      <input
-        type="email"
-        class="form-control"
-        id="inputEmail"
-        aria-describedby="emailHelp"
-        required
-      />
+  <form
+    class="border-top border-bottom p-5 d-flex flex-column align-items-center"
+  >
+    <div class="mb-5 w-100">
+      <div class="mb-3">
+        <label for="inputEmail" class="form-label">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="inputEmail"
+          aria-describedby="emailHelp"
+          placeholder="correo@ejemplo.com"
+          required
+        />
+      </div>
+      <div class="mb-3">
+        <label for="inputPassword" class="form-label">Contraseña</label>
+        <input
+          type="password"
+          class="form-control"
+          id="inputPassword"
+          required
+        />
+      </div>
     </div>
-    <div class="mb-3">
-      <label for="inputPassword" class="form-label">Contraseña</label>
-      <input type="password" class="form-control" id="inputPassword" required />
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="button button--red-outlined">
+      Identifícate
+    </button>
   </form>
 </template>
 
