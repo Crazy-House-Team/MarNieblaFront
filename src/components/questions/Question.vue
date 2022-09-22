c94527a3c89813d0fa73b98c771c2974a1c396df
Mario Rubio <marioastur@gmail.com>
2022-09-22 12:42:48 +0200

    Add question

--- src/components/questions/Question.vue
+++ src/components/questions/Question.vue
@@ -2,8 +2,16 @@
 
 </script>
 <template>
-    <div>
-        <p>Pregunta</p>
+    <div class="d-flex ">
+        <div class="p-2 me-auto">
+            <p>Pregunta</p>
+        </div>
+        <div class="p-2">
+            <button>Editar</button>
+        </div>
+        <div class="p-2">
+            <button>Eliminar</button>
+        </div>
     </div>
 </template>
 <style scoped>
--- src/components/questions/QuestionsList.vue
+++ src/components/questions/QuestionsList.vue
@@ -1,9 +1,20 @@
-<script>
+<script setup>
+import Question from "./Question.vue";
     // Hay que poner un for que leea todas las preguntas
+    // Forma de la tabla questions:
+    /*  'question',
+        'answer_a',
+        'answer_b',
+        'answer_c',
+        'answer_d',
+        'competencies_id',
+        'right_answer',*/
 </script>
 <template>
-    <div>
-    <h1>Listado de todas las preguntas</h1>    
+    <div class="container">
+        <div v-for="index in 5">
+            <Question />   
+        </div>
     </div>
 </template>
 <style scoped>
