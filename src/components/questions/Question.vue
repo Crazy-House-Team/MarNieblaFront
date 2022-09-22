7a9fd8275485419e4c959db3f3192d8251372c77
Mario Rubio <marioastur@gmail.com>
2022-09-22 11:53:58 +0200

    Add questions list

---
+++ src/components/questions/Question.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+<template>
+    <div>
+        <p>Pregunta</p>
+    </div>
+</template>
+<style scoped>
+
+</style>
---
+++ src/components/questions/QuestionsList.vue
@@ -0,0 +1,11 @@
+<script>
+    // Hay que poner un for que leea todas las preguntas
+</script>
+<template>
+    <div>
+    <h1>Listado de todas las preguntas</h1>    
+    </div>
+</template>
+<style scoped>
+
+</style>
