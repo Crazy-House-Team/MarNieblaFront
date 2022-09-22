1efe28d8bce81fb3ba8ad64587a6159f14071720
Francisco <Gimli22@gmail.com>
2022-09-22 20:35:38 +0200

    CreateUserView and component

--- src/components/users/forms/CreateUser.vue
+++ src/components/users/forms/CreateUser.vue
@@ -4,21 +4,21 @@
 </script>
 <template>
     <div class="container">
-        <h2 class="m-3">Nuevo Usuario</h2>
+        <h2 class="m-3">AÑADIR USUARIO</h2>
         <div class="m-3 d-flex justify-content-start align-items-center">
-            <label for="name" class="form-label">Nombre</label>
+            <label for="name" class="form-label"></label>
             <input class="form-control ms-4" id="name" placeholder="Nombre">
         </div>
         <div class="m-3 d-flex justify-content-start align-items-center">
-            <label for="surname" class="form-label">Apellidos</label>
+            <label for="surname" class="form-label"></label>
             <input class="form-control ms-4" id="surname" placeholder="Apellidos">
         </div>
         <div class="m-3 d-flex justify-content-start align-items-center">
-            <label for="email" class="form-label">Email</label>
+            <label for="email" class="form-label"></label>
             <input class="form-control ms-4" id="email" placeholder="Email">
         </div>
         <div class="m-3 d-flex justify-content-start align-items-center">
-            <label for="password" class="form-label">Password</label>
+            <label for="password" class="form-label"></label>
             <input class="form-control ms-4" id="password" placeholder="Password">
         </div>
         <div class="form-check m-3 p-0 d-flex  justify-content-start align-items-center">
@@ -37,11 +37,27 @@
 <style scoped>
     h2{
         text-align: center;
+        font-size: 40px;
+        
     }
     input, textarea{
-    border: solid 2px green;
-    border-radius: 10px;
-}
+    border: solid 2px #3AD86F;
+    border-radius: 30px;
+    }
+    .form-control{
+        
+        margin:30px;
+        width:900px;
+        height:65px;
+        font-size: 24px ;
+        font-weight: bold;
+        color:black;
+        border: solid 5px #3AD86F; 
+        text-align: left;
+
+
+    }
+
     #flexCheckDefault{
         left:30px;
         bottom:3px;
