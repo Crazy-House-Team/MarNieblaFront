e3f0860cfb29e5d1ecf7885d1727f06febdb138a
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-27 12:20:57 +0200

    decouple create user component to createUserView

--- src/views/CreateUserView.vue
+++ src/views/CreateUserView.vue
@@ -1,14 +1,16 @@
-
 <script setup>
-import CreateUser from '../components/users/forms/CreateUser.vue';
-
+import CreateUser from "../components/users/forms/CreateUser.vue";
 </script>
-<template >
-    <div>
-        <CreateUser />
-    </div>
+<template>
+  <div>
+    <h1 class="m-3">AÑADIR USUARIO</h1>
+    <CreateUser />
+  </div>
 </template>
 
 <style scoped>
-    
+h1 {
+  text-align: center;
+  font-size: 40px;
+}
 </style>
