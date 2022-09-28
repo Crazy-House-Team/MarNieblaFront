98cdddc125df7ce0c7a3992d50131d3a3e0aff9d
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-28 15:32:19 +0200

    form edit user

--- src/views/EditUserView.vue
+++ src/views/EditUserView.vue
@@ -1,14 +1,11 @@
-
 <script setup>
-import EditUser from '../components/users/forms/EditUser.vue';
-
+import EditUser from "../components/users/forms/EditUser.vue";
 </script>
-<template >
-    <div>
-      <EditUser />
-    </div>
+<template>
+  <div>
+    <h1 class="m-3">EDITAR USUARIO</h1>
+    <EditUser />
+  </div>
 </template>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
