4487461e342ff1ad37b3e74d34b9c88676d90e6f
Susana M.A <asturias.susi.pro@gmail.com>
2022-09-30 20:37:02 +0200

    Finihed Exam User View

--- src/views/ExamUserView.vue
+++ src/views/ExamUserView.vue
@@ -1,67 +1,79 @@
 
 <script setup>
 
-
-
-
 </script>
 <template >
-    
   <div class="container">
-    <div class="d-flex justify-content-center">
-      <h2>TEST 1 </h2>
-    </div>
     <div>
-      <label for="questionlabel" class="form-label">Pregunta 1</label>
+      <label for="questionlabel" class="pregunta form-label d-flex justify-content-center ">TEST 1 - PREGUNTA 1</label>
       <textarea type="question" class="form-control" id="questionlabel" rows="3"></textarea>
     </div>
-    <h2 class="m-3">Respuestas posibles</h2>
-    <div class="respuestas">
-    <div class="m-3 d-flex justify-content-start align-items-center">
-      <label for="answer_A" class="form-label">A</label>
-      <input class="form-control ms-4" id="answer_A">
+   
+    <div class="respuestas mt-2">
+            <div class="row">
+                <div class="col-6 col-xlm-3 mt-3 d-flex justify-content-start align-items-center">
+                    <label for="answer_A" class="form-label" ></label>
+                    <input class="form-control mt-3 ms-4" id="answer_A" placeholder="A">
+                </div>
+                <div class="col-6 col-xlm-3 mt-3 d-flex justify-content-start align-items-center">
+                    <label for="answer_C" class="form-label"></label>
+                    <input class="form-control  mt-3 ms-4" id="answer_C" placeholder="C">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 col-xlm-3 mt-3 d-flex justify-content-start align-items-center">
+                    <label for="answer_B" class="form-label"></label>
+                    <input class="form-control ms-4 mt-3" id="answer_B" placeholder="B" >
+                </div>
+                <div class="col-6 col-lm-3 mt-3 d-flex justify-content-start align-items-center">
+                    <label for="answer_D" class="form-label"></label>
+                    <input class="form-control ms-4 mt-3" id="answer_D" placeholder="D">
+                </div>
+            </div>
     </div>
-    <div class="m-3 d-flex justify-content-start align-items-center">
-      <label for="answer_B" class="form-label">B</label>
-      <input class="form-control ms-4" id="answer_B">
-    </div>
-    <div class="m-3 d-flex justify-content-start align-items-center">
-      <label for="answer_C" class="form-label">C</label>
-      <input class="form-control ms-4" id="answer_C">
-    </div>
-    <div class="m-3 d-flex justify-content-start align-items-center">
-      <label for="answer_D" class="form-label">D</label>
-      <input class="form-control ms-4" id="answer_D">
-    </div>
- </div>
-    
     <div>
-      <button class="button--purple-outlined">ENVIAR RESPUESTA</button>
+      <button class="button--purple-outlined mt-5">ENVIAR RESPUESTA</button>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped> 
+    
+   h2 {
+    font-size: 25px;
+   }
+
+ .form-label {
+    font-size: 20px;
+    margin: 20px;
+  }
+ .pregunta {
+    color: #F8981D;
+    font-size: 25px;
+    margin:0;
+ }
 
  textarea {
-  border: 6px solid #F8981D;
-  border-radius: 0px;
-  height: 150px;
+   border: 6px solid #F8981D;
+   border-radius: 0px;
+   height: 150px;
   }
-.button--purple-outlined {
-  background-color: #ffffff;
-  border: 6px solid #675979;
-  border-radius: 20px;
-  margin-left: 15%;
-  height: 60px;
-  width: 800px;
-}
+ 
+ .button--purple-outlined {
+   background-color: #ffffff;
+   border: 6px solid #675979;
+   border-radius: 20px;
+   margin-left: 15%;
+   height: 55px;
+   width: 800px;
+ }
 
-input{
+ input{
   background-color: #ffffff;
   border:6px solid #92c954;
   border-radius: 30px;
   height: 80px;
   width: 450px;
-}
+ }
+    
 </style>
