488f7104ddfc7b71471dc2b1a1084bb5a8c1cc86
Susana M.A <asturias.susi.pro@gmail.com>
2022-10-08 17:02:16 +0200

    Create Test List Design

---
+++ src/components/common/CreateTestButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <RouterLink :to="{ name: 'testlist' }">
+      <button class="button-createtest">
+        <h2>Crear Test</h2>
+      </button>
+    </RouterLink>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+.button-createtest {
+  background-color: #92c954;
+  border-radius: 20px;
+  width: 14vw;
+  height: 6vh;
+  border: none;
+  color: white;
+}
+
+.button-createtest:hover {
+  background-color: white;
+  color: #92c954;
+}
+</style>
---
+++ src/views/TestListView.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import BackButton from "../components/common/BackButton.vue";
+  import CreateTestButton from "../components/common/CreateTestButton.vue";
+  import TestRow from "../components/users/forms/TestRow.vue";
+
+</script>
+
+<template>
+   <h1>Gestión de Test</h1> 
+    <div class="justify-content-between d-flex w-100 m-4">
+      <CreateTestButton/>
+      <BackButton />
+    </div>
+    <ul class="list-group-item d-flex justify-content-between align-items-center">
+      <TestRow />
+    </ul>
+</template>
+
+<style scope>
+  h1 {
+    text-align: center;
+  }
+
+  ul {
+    width: 100%;
+  }
+</style>
