521479d9732ae6c4674142d657fcf3bf57dd1523
Susana M.A <asturias.susi.pro@gmail.com>
2022-10-10 14:17:21 +0200

    Logo Header and logout

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,20 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <header class="header">
+   
     <nav class="nav">
+      <div class="logo-title">
+    
       <RouterLink :to="{ name: 'home' }">
         <img
-          src="@/assets/logo_mar-de-niebla-300x156.jpeg"
+          src="@/assets/Iconcolors.jpg"
           alt="logotipo"
           class="header__logo"
         />
       </RouterLink>
+      <h1>Mar de Dudas</h1>
+      </div>
+      <RouterLink class="logout" to="/">Logout</RouterLink>
     </nav>
   </header>
   <main class="main">
@@ -27,11 +33,9 @@ import { RouterLink, RouterView } from "vue-router";
   display: flex;
   justify-content: center;
 }
-
 .header__logo {
   height: 6.8rem;
 }
-
 .nav {
   width: 1280px;
   padding: 1rem 2rem;
@@ -46,5 +50,24 @@ import { RouterLink, RouterView } from "vue-router";
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+h1 {
+  color:white;
+  display:flex;
+  align-items: center;
+  justify-content: espace-around;
+  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
+  font-size: 4rem;
+}
+.logo-title{
+  display:flex;
+  justify-content: space-around;
+}
+.logout {
+display: flex;
+align-items: center;
+font-size:2rem;
+margin-right: 2rem;
 }
 </style>
