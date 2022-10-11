ae3b201f8bfaf0815f607ab613c788c5dad077ba
Mario Rubio <marioastur@gmail.com>
2022-10-12 00:14:03 +0200

    Add filter in users view

--- src/components/users/StudentRow.vue
+++ src/components/users/StudentRow.vue
@@ -1,34 +1,38 @@
 <script setup>
-import EditButton from "../common/EditButton.vue";
-import { defineProps } from "vue";
-import DeleteButton from "../common/DeleteButton.vue";
 
-defineProps({
-  id: Number,
-  username: String,
-  group: String,
-  userRole: Number,
-  role: Number,
-  
-});
+  import EditButton from "../common/EditButton.vue";
+  import { ref, defineProps } from "vue";
+  import DeleteButton from "../common/DeleteButton.vue";
+
+  defineProps({
+    id: Number,
+    name: String,
+    username: String,
+    group: String,
+    userRole: Number,
+    role: Number,
+    groupSearch: String,
+  });
+
 </script>
 
 <template>
-  <li
-    class="list-group-item d-flex justify-content-between align-items-center"
-    v-if="userRole === role"
-  >
-    {{ username }} - Grupo : {{ group }}
+
+  <li v-if="userRole === role && groupSearch=='' || userRole === role && group==groupSearch" class="list-group-item d-flex justify-content-between align-items-center">
+    {{ name }} - {{ username }} - ( Grupo : {{ group }} )
     <span class="badge d-flex">
       <EditButton :id="id" />
       <DeleteButton :id="id" />
     </span>
   </li>
+  
 </template>
 
 <style scoped>
-li {
-  width: 100%;
-  border: 0.2rem solid #92c954;
-}
+
+  li {
+    width: 100%;
+    border: 0.2rem solid #92c954;
+  }
+
 </style>
--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,24 +1,28 @@
 <script setup>
 
-import { onMounted, ref } from "vue";
-import StudentRow from "../components/users/StudentRow.vue";
-import { getAction } from "../services/apiRequests";
-import BackButton from "../components/common/BackButton.vue";
-import CreateUserButton from "../components/common/CreateUserButton.vue";
+  import { onMounted, ref } from "vue";
+  import StudentRow from "../components/users/StudentRow.vue";
+  import { getAction } from "../services/apiRequests";
+  import BackButton from "../components/common/BackButton.vue";
+  import CreateUserButton from "../components/common/CreateUserButton.vue";
 
-defineProps({
-  role: String,
-});
+  defineProps({
+    role: String,
+  });
 
-const userList = ref([]);
-const numOfUsersInList = ref(0);
-onMounted(async () => {
-  userList.value = await getAction("users");
-  numOfUsersInList.value = userList.value.data.length;
-});
+  const userList = ref([]);
+  const numOfUsersInList = ref(0);
+  let groupSearch = ref("");
+
+  onMounted(async () => {
+    userList.value = await getAction("users");
+    numOfUsersInList.value = userList.value.data.length;
+  });
+
 </script>
 
 <template>
+
   <div class="wrapper">
     <div class="justify-content-around d-flex w-100 m-4">
       <CreateUserButton />
@@ -26,20 +30,28 @@ onMounted(async () => {
       <h1 v-else>Lista de profesores</h1>
       <BackButton toRoute="/admin" />
     </div>
+
+    <div class="d-flex">
+        <input v-model=groupSearch class="form-control mb-4 mr-sm-2" type="search" placeholder="General">
+    </div>
+
+    
     <ul class="list-group" v-if="numOfUsersInList > 0">
       <StudentRow
         v-for="(user, index) in userList.data"
         :key="index"
         :id="user.id"
         :name="user.name"
-        :username="user.name"
+        :username="user.username"
         :group="user.group"
         :userRole="user.isAdmin"
         :role="Number(role)"
+        :groupSearch = groupSearch
       />
     </ul>
     <p class="p__warning" v-else>No existe ningún usuario.</p>
   </div>
+
 </template>
 
 <style scoped>
